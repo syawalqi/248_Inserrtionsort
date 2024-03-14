5685712857a5dade9d658cc5221b368095f7a5e6
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-14 15:05:08 +0700

    menambahkan step3

--- Insertionsort/Insertionsort.cpp
+++ Insertionsort/Insertionsort.cpp
@@ -3,13 +3,17 @@ using namespace std;
 
 
 void insertionsort() {
-    int i, j, n;
-    int arr[20];
+    int i, j, n, temp;
+    int arr[40];
     
     for (i = 1; i <= n - 1; i++) {// step 1
-        
+        temp = arr[i]; //step 2
+
+        j = i - 1; //step3
+
     }
 }
+
 
 int main()
 {
