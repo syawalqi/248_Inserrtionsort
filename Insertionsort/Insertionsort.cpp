034187ffbfab324f704caa346bd3c64682479eae
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-14 14:34:07 +0700

    Add project files.

---
+++ Insertionsort/Insertionsort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+void insertionsort() {
+    int i, j, n;
+    int arr[20];
+    
+    for (i = 1; i <= n - 1; i++) {// step 1
+
+    }
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
